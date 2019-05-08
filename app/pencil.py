76a70943bf61fea1db8fafa4cf1da0b04731ee72
levi <ugp@ugps-MacBook.local>
2019-05-07 23:04:19 -0400

    writing initial write method

--- app/pencil.py
+++ app/pencil.py
@@ -7,4 +7,17 @@ class Pencil():
 		self.eraser_left = eraser_durability
 		self.written_text = ''
 
+	def write(self, text):
+		characters = list(text)
+		count_blank = characters.count(' ')
+		count_diff = self.lead_left - (len(characters) - count_blank)
+		if count_diff < 0:
+			self.lead_left = 0
+		else:
+			self.lead_left = count_diff
 
+		print(characters)
+
+		if not self.written_text.endswith(' ') and self.written_text != '':
+			self.written_text += ' '
+		self.written_text += ''.join(characters)
--- test/test_pencil.py
+++ test/test_pencil.py
@@ -5,41 +5,41 @@ from app.pencil import Pencil
 class TestPencil:
 	
 	def test_write(self):
-		pencil = new Pencil(50, 50)
+		pencil = Pencil(50, 50)
 		pencil.write('Hello world')
-	    assert pencil.written_string == 'Hello world'
-	    assert pencil.lead_left == 40
+		assert pencil.written_text == 'Hello world'
+		assert pencil.lead_left == 40
 
 	def test_write_twice(self):
-		pencil = new Pencil(50, 50)
+		pencil = Pencil(50, 50)
 		pencil.write('Hello world')
-	    assert pencil.written_string == 'Hello world'
-	    assert pencil.lead_left == 40
-	    pencil.write('what a great day')
-	    assert pencil.written_string == 'Hello world what a great day'
-	    assert pencil.lead_left == 27
+		assert pencil.written_text == 'Hello world'
+		assert pencil.lead_left == 40
+		pencil.write('what a great day')
+		assert pencil.written_text == 'Hello world what a great day'
+		assert pencil.lead_left == 27
 
 	def test_sharpen(self):
-		pencil = new Pencil(50, 50)
+		pencil = Pencil(50, 50)
 		pencil.write('Hello world')
 		pencil.sharpen()
 		assert pencil.lead_durability == 50
 
 	def test_erase(self):
-		pencil = new Pencil(50, 50)
+		pencil = Pencil(50, 50)
 		pencil.write('Hello world')
 		pencil.erase('world')
 		assert pencil.eraser_left == 45
 		assert pencil.written_text == 'Hello '
 
 	def test_edit(self):
-		pencil = new Pencil(50, 50)
+		pencil = Pencil(50, 50)
 		pencil.write('Hello world')
 		pencil.edit(6, 'Susan')
 		assert pencil.written_text == 'Hello @@@@@'
 
 	def test_edit_multiple_whitespace(self):
-		pencil = new Pencil(50, 50)
+		pencil = Pencil(50, 50)
 		pencil.write('Hello    world')
 		pencil.edit(6, 'Susan')
 		assert pencil.written_text == 'Hello Sus@@rld'
