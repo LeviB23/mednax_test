49cea9ba998a837a15a695aacc763b7d4977ebfd
levi <ugp@ugps-MacBook.local>
2019-05-07 23:22:35 -0400

    rewriting method to be easier to read vs efficient

--- app/pencil.py
+++ app/pencil.py
@@ -9,15 +9,29 @@ class Pencil():
 
 	def write(self, text):
 		characters = list(text)
-		count_blank = characters.count(' ')
-		count_diff = self.lead_left - (len(characters) - count_blank)
-		if count_diff < 0:
-			self.lead_left = 0
-		else:
-			self.lead_left = count_diff
+		# count_blank = characters.count(' ')
+		# count_diff = self.lead_left - (len(characters) - count_blank)
+		# if count_diff < 0:
+		# 	self.lead_left = 0
+		# 	del characters[abs(count_diff):len(characters)]
+		# 	blank_list = [' ' for i in range(abs(count_diff))]
+			# characters = characters + blank_list
+		# else:
+		# 	self.lead_left = count_diff
 
-		print(characters)
+		# print(characters)
 
-		if not self.written_text.endswith(' ') and self.written_text != '':
+		# if not self.written_text.endswith(' ') and self.written_text != '':
+		# 	self.written_text += ' '
+		# self.written_text += ''.join(characters)
+		if not self.written_text.endswith(' ') and self.written_text != '' and self.lead_left > 0:
 			self.written_text += ' '
-		self.written_text += ''.join(characters)
+
+		for character in characters:
+			if self.lead_left > 0:
+				self.written_text += character
+			else:
+				self.written_text += ' '
+
+			if character != ' ' and self.lead_left > 0:
+				self.lead_left -= 1
